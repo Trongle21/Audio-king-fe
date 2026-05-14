02502e2adfabff3690a61e3a020f0d63fac59c97
trong.lehuu <trongleele2@gmail.com>
2026-05-14 10:34:44 +0700

    update size

--- components/organisms/ProductFilters.tsx
+++ components/organisms/ProductFilters.tsx
@@ -63,7 +63,7 @@ function FilterFields({
         <select
           id="categoryId"
           name="categoryId"
-          className="w-3/4 xl:w-full rounded-md border bg-background px-2 py-2 text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring"
+          className="w-3/4 h-10 xl:w-full rounded-md border bg-background px-2 py-2 text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring"
           value={state.categoryId}
           onChange={(e) =>
             setState((s) => ({ ...s, categoryId: e.target.value }))
